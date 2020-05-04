9a169ad002a552820cb80abab9aa69401c0775eb
Valerie Chau <valeriechau@Valeries-Air.attlocal.net>
2020-05-03 23:31:48 -0700

    initial commit

---
+++ pepsearch.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import gzip
+import sys
+
+# Write a program that finds peptidies within protein sequences
+# Command line:
+#	python3 pepsearch.py IAN
+
+
+assert(len(sys.argv)==3)
+file = sys.argv[1]
+pattern = sys.argv[2]
+
+def read_fasta(filename):   #taken from 12_functions.py
+	name = None
+	seqs = []
+
+	fp = None
+	if filename == '-':
+		fp = sys.stdin
+	elif filename.endswith('.gz'):
+		fp = gzip.open(filename, 'rt')
+	else:
+		fp = open(filename)
+
+	for line in fp.readlines():
+		line = line.rstrip()
+		if line.startswith('>'):
+			if len(seqs) > 0:
+				seq = ''.join(seqs)
+				yield(name, seq)
+				name = line[1:]
+				seqs = []
+			else:
+				name = line[1:]
+		else:
+			seqs.append(line)
+	yield(name, ''.join(seqs))
+	fp.close()
+
+count = 0
+for name, seq in read_fasta(file):
+	if pattern in seq:
+		print(pattern)
+
+
+
+
+
+"""
+python3 pepsearch.py proteins.fasta.gz IAN | wc -w
+	43
+"""
